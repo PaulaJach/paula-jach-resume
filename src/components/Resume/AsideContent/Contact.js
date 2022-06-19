9a01fec921efc0bcad963f4921480128e9b8b86f
Paula Jach <paulajach92@gmail.com>
2022-06-19 14:25:21 +0200

    installed fontawesome library to add icons in Contact section

--- src/components/Resume/AsideContent/Contact.js
+++ src/components/Resume/AsideContent/Contact.js
@@ -1,15 +1,34 @@
+import styled from "styled-components";
 import { contactData } from "../../../assets/data/ContactData";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faEnvelope,
+  faPhoneVolume,
+  faLaptopCode,
+} from "@fortawesome/free-solid-svg-icons";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 const Contact = () => {
   return (
     <div>
       <h4>CONTACT</h4>
-      {contactData.map((contact, id) => (
-        <li key={id}>
-          <h5>{contact.name}</h5>
-          <p>{contact.value}</p>
-        </li>
-      ))}
+
+      {contactData.map((contact, id) => {
+        const { name, value, href } = contact;
+
+        const icon =
+          (name === "telephone" && <FontAwesomeIcon icon={faPhoneVolume} />) ||
+          (name === "gitHub" && <FontAwesomeIcon icon={faGithub} />) ||
+          (name === "linkedIn" && <FontAwesomeIcon icon={faLinkedin} />) ||
+          (name === "email" && <FontAwesomeIcon icon={faEnvelope} />) ||
+          (name === "portfolio" && <FontAwesomeIcon icon={faLaptopCode} />);
+        return (
+          <li key={id}>
+            {icon}
+            <p>{name}</p>
+          </li>
+        );
+      })}
     </div>
   );
 };
--- src/styles.js
+++ src/styles.js
@@ -4,6 +4,7 @@ export const setColor = {
   primaryColor: "#234515",
   secondaryColor: "#575859",
   footerColor: "#8c8e91",
+  iconColor: "#2C6415",
 };
 
 // export const setFont = {
